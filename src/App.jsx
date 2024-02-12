d45a88fdda3059e99fc69549b038791aa0c63e57
Dor Mizrahi <109686777+DorMizrahii@users.noreply.github.com>
2024-02-12 19:24:29 +0200

    update upper case to the Component import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
 import MonthlyReport from "./Components/MonthlyReport";
-import AddCaloriesForm from "./components/AddCaloriesForm";
+import AddCaloriesForm from "./Components/AddCaloriesForm"
 import { fetchEntries } from "./hooks/fetchEntries";
 
 function App() {
